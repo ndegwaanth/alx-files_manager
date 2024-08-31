118c48ef7b97476362cde255259492daccabed4d
Anthony <ndegwaanthony300@gmail.com>
2024-08-31 20:23:50 +0300

    created a file redis.js that contains the class RedisClient

---
+++ utils/redis.js
@@ -0,0 +1,52 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    // Create a Redis client instance
+    this.client = redis.createClient();
+
+    // Handle errors
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+  }
+
+  // Check if Redis connection is alive
+  isAlive() {
+    return this.client.connected;
+  }
+
+  // Get value from Redis by key
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) return reject(err);
+        resolve(value);
+      });
+    });
+  }
+
+  // Set value in Redis with an expiration time
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) return reject(err);
+        resolve(reply);
+      });
+    });
+  }
+
+  // Delete value from Redis by key
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) return reject(err);
+        resolve(reply);
+      });
+    });
+  }
+
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
